f557da8bdbc0032c5504df79cbdf54f339cb0670
shivammahajan31 <shivammahajan221@gmail.com>
2025-06-04 09:45:04 +0530

    ll

--- task6.js
+++ task6.js
@@ -1,21 +1,29 @@
 var points = [10,15,20,30,5];
-var pointscopy = [...points,50,75];
+var pointscopy = [...points,50,75,];
 
 console.log(points,pointscopy)
 
+var x = [20,30,60,40,70]
+var y =[" a =" ]
+var z = [...y,...x,...y,...x,1000,2000,3000]
+console.log(z)
 
 const numbers = [0, 10, 20];
-const newnumbers = [...numbers.map(n => n + 10)];
-console.log(newnumbers); 
+const numbersaddition = [...numbers,30,40,50,60,...numbers].map(n => n + 10);
+console.log(numbersaddition); 
 
 
 
 const marks = [70, 45, 90, 30, 55, 20, 88];
+const [A, B, c, ...restMarks] = marks;
 
 function filterMarks(first, ...rest) {
     const allMarks = [first, ...rest];
-    const filtered = allMarks.filter(mark => mark > 50);
-    console.log("Filtered Marks:", filtered);
+    const passed = allMarks.filter(mark => mark > 50);
+    const fail = allMarks.filter(mark => mark < 50);
+    console.log("passed Marks:", passed);
+    console.log("failed Marks:", fail);
 }
 
 filterMarks(...marks);
+filterMarks(A,B);
