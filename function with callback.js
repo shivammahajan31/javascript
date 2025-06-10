1f4ae803e492134e430030ea26b7aa757859007b
shivammahajan31 <shivammahajan221@gmail.com>
2025-06-10 10:49:31 +0530

    ssss

--- function with callback.js
+++ function with callback.js
@@ -6,3 +6,16 @@
 //let doubled = processArray(numbers, sum);
 
 //console.log(doubled);
+ 
+
+let number = [1,2,3,4,5,6,7,8]
+
+ var a = number.map(num => num > 5);
+ console.log(a);544
+
+ 
+   let b = prompt("enter ");
+   let c = prompt("enter 2")
+   let d = [b,c];
+  const evens = d.filter(num => num % 2 === 0);
+  console.log(evens);
